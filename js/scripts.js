7e4a6af798288ca1d7583e06d13ce2fea7c141e4
Sean Wilson <paintballsean@gmail.com>
2020-08-29 01:44:19 -0700

    Commit adds add pizza prototype to pizzaorder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 // Business Logic for Devil's Pizza Order
 
 function PizzaOrder() {
-  this.devilPizzas= [];
+  this.orderPizzas = [];
+  this.demonNumber = 0;
+}
+
+PizzaOrder.prototype.addDevilsPizza = function(pizzaOrder) {
+  pizzaOrder.demonNumber = this.demonNumber();
 }
 
 // Business Logic for Devil's Pizza
 
-function 
-
-PizzaOrder.prototype.addDevilsPizza = function(devilPizzas) {
-  devilPizzas.demonNumber
+function PizzaInfo(size,toppings) {
+  this.size = size;
+  this.toppings = toppings;
 }
+
 // User Experience Element
 
+let pizzaOrder = new PizzaOrder();
 $(document).ready(function() {
-  attachContactListeners();
-  $("form#pizzForm").submit(function(event) {
-    event.preventDefault();
+  event.preventDefault();
+  $("form#pizzaForm").submit(function(event) {
+    
 
   });
 });
