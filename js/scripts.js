15a10a13a68698efa15f863d45794a6e61bfe64f
Sean Wilson <paintballsean@gmail.com>
2020-08-29 15:04:53 -0700

    Clearing out rough idea that I had on paper and  trying to simplify it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,6 @@
 // Business Logic for Devil's Pizza Order
 
-function PizzaOrder() {
-  this.orderPizzas = [];
-  this.demonNumber = 0;
-}
-
-PizzaOrder.prototype.addDevilsPizza = function(pizzaOrder) {
-  pizzaOrder.demonNumber = this.demonNumber();
-}
-
-// Business Logic for Devil's Pizza
-
-function PizzaInfo(size,toppings) {
-  this.size = size;
-  this.toppings = toppings;
-}
+.
 
 // User Experience Element
 
@@ -23,6 +9,5 @@ $(document).ready(function() {
   event.preventDefault();
   $("form#pizzaForm").submit(function(event) {
     
-
   });
 });
