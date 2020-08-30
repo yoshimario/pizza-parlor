655ee8de06c6931d1321dafc9f92de7800c57b62
Sean Wilson <paintballsean@gmail.com>
2020-08-29 21:22:00 -0700

    Simplified functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 // Business Logic for Devil's Pizza Order
 
-.
+function DevilsPizzas() {
+  this.demonNumber = 0;  // Order Number
+  this.devilsPizzas = [];
+}
+
+DevilsPizzas.prototype.devilsPrice = function(devilsPizza) {
+  let demonsTotal = 0;
+  let devilsPrice = [100, 80, 20];
+  let devilsSize = ["biteSized", "demonsGlutton", "devilsFortress"];
+  for (let d = 0; d < devilsSize.length; d++) {
+    if (devils.size === devilsSize[d]) {
+      demonsTotal = devilsPrice[d];
+    }
+  }
+  demonsTotal (devilsPizza.devilsToppings.length * 202.50);
+  return demonsTotal;
+}
 
 // User Experience Element
+function devilsPizza {
+  this.devilsSize = devilsSize;
+  this.devilsToppings = devilsToppings;
+}
 
-let pizzaOrder = new PizzaOrder();
 $(document).ready(function() {
   event.preventDefault();
   $("form#pizzaForm").submit(function(event) {
