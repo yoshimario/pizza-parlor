4c0f06ad4220668c091618fdc89565623029d8d3
Sean Wilson <paintballsean@gmail.com>
2020-08-30 18:18:35 -0700

    Commit changes code to try another method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,51 @@
 // Business Logic for Devil's Pizza Order
 
-function DemonsOrder(devilsSize, devilsToppings) {
+function DevilsPizza(devilsSize, devilsToppings) {
   this.devilsSize = devilsSize;
   this.devilsToppings = devilsToppings;
-  this.devilsPrice = devilsPrice;
+  this.devilsPrice = 0;
 }
 
-DemonsOrder.prototype.devilsPrice = function() {
-  let devilsPrice = [240,120,30];
-  let devilsSize = ["biteSized","demonsGlutton","devilsFortress"];
+  DevilsPizza.prototype.devilsPizzaPrice = function() {
 
-  if (devilsSize === "biteSized") {
-    this.devilsPrice === devilsPrice[1];
-  } else if (devilsSize === "demonsGlutton") {
-    this.devilsPrice === devilsPrice[2];
-  } else if (devilsSize === "devilsFortress") {
-    this.devilsPrice === devilsPrice[3];
+  if (this.devilsSize === "biteSized") {
+    this.devilsPrice += 280;
+  } else if (this.devilsSize === "demonsGlutton") {
+    this.devilsPrice += 140;
+  } else if (this.devilsSize === "devilsFortress") {
+    this.devilsPrice += 40;
   } else {
     alert("Yo demon order your selection!");
-    this.devilsPrice = 0;
+    this.devilsPrice = this.devilsPrice;
   }
   console.log("devilsPrice");
-  this.devilsPrice = this.devilsToppings.length * 25.40 + this.devilsPrice;
+  this.devilsPrice = this.devilsToppings.devils.length * 25.40 + this.devilsPrice;
   }
   return this.devilsPrice;
-};
+}
 
-function DevilsToppings (devilsToppings) {
-  this.devilsToppings = devilsToppings;
+function DevilsToppings (devils) {
+  this.devils = devils;
 }
 
 // User Experience Element
-let demonsOrder = new DemonsOrder();
+let devilsPizza = new DevilsPizza();
 let devilsToppings = new DevilsToppings();
 
 $(document).ready(function() {
   $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
-    $(".demonOrder").show();
-  })
-  });
-});
+    $(".demonOrderTotal").show();
+    let devilsToppings = new DevilsToppings(inputtedVeggies, inputtedMeat, inputtedSauces, inputtedCheese);
+    let demonsOrder = new DemonsOrder(inputtedSize, inputtedType, pizzaToppings);
+    if (inputtedSize === "error") {
+      alert("Please choose a size");
+    } else if (inputtedType === "error") {
+      alert("Please choose a type of crust");
+    } else {
+      $("input#cheese").each(function() {
+        if ($(this).is(':checked')) {
+          let checked = ($(this).val());
+          inputtedCheese.push(checked);
+        }
+      });
